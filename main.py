a615497c09cca5ba2fae836597044d8cf2f559c5
DarkFeed2005 <kalanayasassrisathruwan123@gmail.com>
2025-06-28 21:07:28 +0530

    main.py

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ service_provider = phonenumbers.parse(number)
 print(carrier.name_for_number(service_provider, "en"))
 
 from opencage.geocoder import OpenCageGeocode
+geocoder = OpenCageGeocode(key)
+query = str(number_location)
+results = geocoder.geocode(query)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(f"Latitude: {lat}, Longitude: {lng}")
+
+map_location = folium.Map(location=[lat, lng], zoom_start=10)
+folium.Marker([lat, lng], popup=number_location).add_to(map_location)
+map_location.save("location_map.html")
+print("Map has been saved to location_map.html")
