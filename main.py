3608027ad2c28368537c5da3112c617c77dab690
DarkFeed2005 <kalanayasassrisathruwan123@gmail.com>
2025-06-28 20:57:46 +0530

    main.py

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from phonenumbers import geocoder
 from test import number
 import folium 
 
+key = "eb927c8c90184db5a9db0835499dd54a"  # Replace with your actual OpenCage API key
+
 check_number = phonenumbers.parse(number)  
 number_location = geocoder.description_for_number(check_number, "en") 
 print(f"Location of the number {number} is: {number_location}") 
 
-from phone_numbers import carrier
+from phonenumbers import carrier
 service_provider = phonenumbers.parse(number)
 print(carrier.name_for_number(service_provider, "en"))
 
+from opencage.geocoder import OpenCageGeocode
